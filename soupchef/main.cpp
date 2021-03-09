a69fca594d8601e7bd09592c46e059f63c3d1cd4
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-03-09 11:36:41 +0100

    Read .obj file into memory - vertices and faces

--- soupchef/main.cpp
+++ soupchef/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <list>
+#include <vector>
 #include "DCEL.hpp"
 
 // forward declarations; these functions are given below main()
@@ -7,7 +11,26 @@ void DemoDCEL();
 void printDCEL(DCEL & D);
 
 //import from hw1 code
-
+void read(std::ifstream &stream_in, std::vector<std::vector<double>> &v, std::vector<std::vector<unsigned int>> &f) {
+    if (stream_in.is_open()) {
+        std::string line;
+        while (getline(stream_in, line)) {
+            std::istringstream iss(line);
+            std::string word;
+            iss >> word;
+            if (word == "v") {
+                std::vector<double> coordinates;
+                while (iss >> word)
+                    coordinates.push_back(std::stof(word));
+                if (coordinates.size() == 3) v.push_back({coordinates[0], coordinates[1], coordinates[2]});
+//else vertices.push_back(Point());
+            } else if (word == "f") {
+                f.push_back(std::vector<unsigned int>());
+                while (iss >> word) f.back().push_back((unsigned int) std::stoul(word));
+            }
+        }
+    }
+}
 /* 
   Example functions that you could implement. But you are 
   free to organise/modify the code however you want.
@@ -36,18 +59,94 @@ void exportCityJSON(DCEL & D, const char *file_out) {
 
 
 int main(int argc, const char * argv[]) {
-  const char *file_in = "bk_soup.obj";
-  const char *file_out = "bk.json";
+  const char *file_in = "/home/konstantinos/Desktop/TUDelft-Courses/Q3/GEO1004/hw2/bk_soup.obj";
+  const char *file_out = "../cube.json";
+
+
+
 
   // Demonstrate how to use the DCEL to get you started (see function implementation below)
   // you can remove this from the final code
-  DemoDCEL();
+  //DemoDCEL();
 
   // create an empty DCEL
   DCEL D;
-
   // 1. read the triangle soup from the OBJ input file and convert it to the DCEL,
-  
+
+  //~~~~~~~~~~~~~~~~~~~~~ 09-03-2021 Read .obj file into memory - vertices and faces ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    std::vector<std::vector<double>> vertices;
+    std::vector<std::vector<unsigned int>> faces;
+    std::ifstream stream_in;
+    stream_in.open(file_in);
+    read(stream_in,vertices,faces);
+  //~~~~~~~~~~~~~~~~~~~~~ 09-03-2021 Read .obj file into memory - vertices and faces ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+    for (unsigned int i = 0; i < faces.size(); i++) {
+        Vertex *v0 = D.createVertex(vertices[(faces[i][0])-1][0], vertices[(faces[i][0])-1][1], vertices[(faces[i][0])-1][2]);
+        Vertex *v1 = D.createVertex(vertices[(faces[i][1])-1][0], vertices[(faces[i][1])-1][1], vertices[(faces[i][1])-1][2]);
+        Vertex *v2 = D.createVertex(vertices[(faces[i][2])-1][0], vertices[(faces[i][2])-1][1], vertices[(faces[i][2])-1][2]);
+
+        HalfEdge* e0 = D.createHalfEdge();
+        HalfEdge* e1 = D.createHalfEdge();
+        HalfEdge* e2 = D.createHalfEdge();
+        HalfEdge* e3 = D.createHalfEdge();
+        HalfEdge* e4 = D.createHalfEdge();
+        HalfEdge* e5 = D.createHalfEdge();
+
+        Face* fi = D.createFace();
+
+        e0->origin = v0;
+        e0->destination = v1;
+        e0->twin = e3;
+        e0->next = e1;
+        e0->prev = e2;
+        e0->incidentFace = fi;
+
+        e3->origin = v1;
+        e3->destination = v0;
+        e3->twin = e0;
+        e3->next = e5;
+        e3->prev = e4;
+
+        /*
+        If a half-edge is incident to 'open space' (ie not an actual face with an exterior boundary),
+        we use the infiniteFace which is predifined in the DCEL class
+        */
+        e3->incidentFace = D.infiniteFace();
+
+        e1->origin = v1;
+        e1->destination = v2;
+        e1->twin = e4;
+        e1->next = e2;
+        e1->prev = e0;
+        e1->incidentFace = fi;
+
+        e4->origin = v2;
+        e4->destination = v1;
+        e4->twin = e1;
+        e4->next = e3;
+        e4->prev = e5;
+        e4->incidentFace = D.infiniteFace();
+
+        e2->origin = v2;
+        e2->destination = v0;
+        e2->twin = e5;
+        e2->next = e0;
+        e2->prev = e1;
+        e2->incidentFace = fi;
+
+        e5->origin = v0;
+        e5->destination = v2;
+        e5->twin = e2;
+        e5->next = e4;
+        e5->prev = e3;
+        e5->incidentFace = D.infiniteFace();
+
+        fi->exteriorEdge = e0;
+    }
+
+    printDCEL(D);
   // 2. group the triangles into meshes,
   
   // 3. determine the correct orientation for each mesh and ensure all its triangles 
