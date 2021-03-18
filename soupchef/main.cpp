4a03f96060e04dbaf9cd9988d971c473fc7d2cf3
Konstantinos Pantelios <konstantinospantelios@yahoo.com>
2021-03-18 23:40:00 +0100

    TODO: centroid

--- soupchef/main.cpp
+++ soupchef/main.cpp
@@ -487,7 +487,7 @@ void mergeCoPlanarFaces(DCEL & D, DCEL& tempD) {
                     c++;
                     continue;
                     }
-                if (angle(curr_norm,neigh_norm) < 1){
+                if (angle(curr_norm,neigh_norm) <= 1){
 
 
                     //if(e_start == e){std::cout<<"maybe\n";continue;} //uncoment to run it but without merges.
@@ -531,8 +531,8 @@ void exportCityJSON(DCEL & D, const char *file_out) {
 
 
 int main(int argc, const char * argv[]) {
-    const char *file_in = "/home/konstantinos/Desktop/TUDelft-Courses/Q3/GEO1004/hw2/cube_soup.obj";
-    const char *file_out = "/home/konstantinos/Desktop/TUDelft-Courses/Q3/GEO1004/hw2/cube.json";
+    const char *file_in = "/home/konstantinos/Desktop/TUDelft-Courses/Q3/GEO1004/hw2/bk_soup.obj";
+    const char *file_out = "/home/konstantinos/Desktop/TUDelft-Courses/Q3/GEO1004/hw2/bk.json";
 
 
     DCEL tempD;
@@ -623,7 +623,7 @@ int main(int argc, const char * argv[]) {
  //mesh_faces contain only those faces that difine the specific mesh.for (auto const& face : D.faces()){
         auto minc = cornerpoints(mesh_vertices[0], "min");
         auto maxc = cornerpoints(mesh_vertices[0], "max");
-    std::vector<double> o = {-10.0 * abs(round(minc[0]))/*minc[0]+(maxc[0] - minc[0]*/, ((maxc[1] - minc[1]) / 2) + minc[1], (maxc[2]*1.5) + minc[2]+2};
+    std::vector<double> o = {-10.0 * abs(round(minc[0])), ((maxc[1] - minc[1]) / 2) + minc[1], (maxc[2]*1.5) + minc[2]+2};
     std::vector<double> d = {D.faces().back()->exteriorEdge->origin->x,
                  D.faces().back()->exteriorEdge->origin->y,
                  D.faces().back()->exteriorEdge->origin->z}; //destination of ray -> a vertex of the last face of the mesh.
@@ -648,11 +648,15 @@ int main(int argc, const char * argv[]) {
 
     mesh_count++;
 }
+
+
             for (auto const& e:D.halfEdges()){
                 if (e->hasDanglingLink()) {
                     e->incidentFace=e->twin->incidentFace;
                 }
             }
+
+
 printDCEL(D);
 
 
